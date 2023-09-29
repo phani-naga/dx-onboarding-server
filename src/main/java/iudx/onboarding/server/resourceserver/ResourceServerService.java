ffee9cc126143a75faf8093df87ef073251bb220
pranavrd <pranav.doshetty@datakaveri.org>
2023-09-29 12:10:00 +0530

    added resource server service

---
+++ src/main/java/iudx/onboarding/server/resourceserver/ResourceServerService.java
@@ -0,0 +1,20 @@
+package iudx.onboarding.server.resourceserver;
+
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.ProxyGen;
+import io.vertx.codegen.annotations.VertxGen;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+
+@VertxGen
+@ProxyGen
+public interface ResourceServerService {
+
+  Future<JsonObject> createAdapter(String id, String token);
+  @GenIgnore
+  static ResourceServerService createProxy(Vertx vertx, String address) {
+    return new ResourceServerServiceVertxEBProxy(vertx, address);
+  }
+
+}
---
+++ src/main/java/iudx/onboarding/server/resourceserver/package-info.java
@@ -0,0 +1,7 @@
+@ModuleGen(
+    groupPackage = "iudx.onboarding.server.resourceserver",
+    name = "iudx-onboarding-server-resource-service",
+    useFutures = true)
+package iudx.onboarding.server.resourceserver;
+
+import io.vertx.codegen.annotations.ModuleGen;
